7b0887ee3a5c0d6a245605097e19f6ec860315a7
2305hua <117661994+2305hua@users.noreply.github.com>
2023-03-08 14:07:03 +0800

    Add files via upload

--- List.c
+++ List.c
@@ -3,7 +3,7 @@
 #include<stdbool.h>
 #define MAXSIZE 10
 /*
-顺序表  
+顺序表  2023.3.5
 */
 struct Arr//定义一个结构体
 {
@@ -13,6 +13,10 @@ struct Arr//定义一个结构体
 /*
 ----------------------------------------------------------------------------------------------------------------
 */
+bool Insert(struct Arr *parr,int pos,int e);//插入函数：pos为插入位置，e为插入的元素 
+bool Delete(struct Arr *parr,int pos);//删除函数：删去给定位置第pos的元素； 
+int LocateElem(struct Arr *parr,int e);//查找函数：在表中寻找值与e相等的元素； 
+bool GetElem(struct Arr *parr,int pos,int *e);//取值函数：选取表中第pos个位置的元素赋值给e 
 void delete_same3(struct Arr *parr,int item);//使用whlie循环 
 void delete_same2(struct Arr *parr,int same);//删除表中与item值相同的元素 ;使用的是for循环 
 //void delete_same1(struct Arr *parr);
@@ -28,6 +32,7 @@ bool is_empty (struct Arr *parr);//判断表是否为空
 int main()
 {
 	struct Arr arr;
+	int e = 0;
 	InitList(&arr);//对结构体变量arr初始化
 	ListInsert(&arr,1);/*对顺序表赋值*/
 	ListInsert(&arr,4);
@@ -45,12 +50,62 @@ int main()
 	delete_same3(&arr,2);//参考智慧树视频的代码 
 	printf("删除后\n");
 	ShowList(&arr);
+	//GetElem(&arr,3,&e);
+	//printf("e = %d\n",e);
+	//LocateElem(&arr,2);
+	//Delete(&arr,2);
+	//ShowList(&arr);
+	printf("插入后：\n");
+	Insert(&arr,1,9);
+	ShowList(&arr);
 	return 0;
 }
 
 /*
 -----------------------------------------------------------------------------------------------------------------
 */
+bool Insert(struct Arr *parr,int pos,int e)//插入函数：pos为插入位置，e为插入的元素
+{
+	int i = parr->len;
+	if(pos < 1 || pos >parr->len || is_full(parr))//判断插入位置合法性及表是否已满 
+		return false;
+	for(i = parr->len;i >= pos;i--)
+	{
+		parr->pelem[i] = parr->pelem[i-1];
+	}
+	parr->pelem[pos-1] = e;
+	parr->len++;
+	return true;
+}
+bool Delete(struct Arr *parr,int pos)//删除函数：删去给定位置第pos的元素； 
+{
+	if(pos < 1 || pos > parr->len)//如果删除位置不合法则返回错误； 
+	return false;
+	for(pos;pos < parr->len;pos++)
+	{
+		parr->pelem[pos-1]=parr->pelem[pos];
+	}
+	parr->len--;
+}
+int LocateElem(struct Arr *parr,int e)//查找函数 
+{
+	int i = 0;
+	for(i = 0;i < parr->len;i++)
+	{
+		if(parr->pelem[i] == e)
+		{
+			return i+1;//查找成功返回该元素所在的位置 
+		}
+	}
+	return 0;//查找失败返回0 
+}
+bool GetElem(struct Arr *parr,int pos,int *e)//取值函数 
+{
+	if(pos<1 || pos>(parr->len))
+		return false;
+	*e = parr->pelem[pos-1];
+	return true;
+}
 void ShowList(struct Arr *parr)//展示表中数据 
 {
 	int i;
@@ -181,4 +236,5 @@ bool ListInsert(struct Arr *parr,int data)//追加数据
 		return true;
 	}
 }
+
 
